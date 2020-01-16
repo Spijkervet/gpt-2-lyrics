0b9dded2e2f9d165f01c873fe7deadefb1418bf6
Janne Spijkervet <janne.spijkervet@gmail.com>
2020-01-16 01:39:05 +0100

    Added redis

--- generate.py
+++ generate.py
@@ -1,27 +1,52 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
+from flask_cors import CORS
 from json import dumps
-from flask.ext.jsonpify import jsonify
+
+from celery import Celery
+from celery.utils.log import get_task_logger
+from celery.signals import worker_init, worker_process_init
+
+import celery
 
 import gpt_2_simple as gpt2
 
 app = Flask(__name__)
-api = Api(app)
+# Celery configuration
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
-class Lyrics(Resource):
-    def get(self):
-        generated = gpt2.generate(sess)
-        print(generated)
-        return 'hi'
+# Initialize Celery
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+CORS(app)
 
+sess = None
 
-api.add_resource(Lyrics, '/lyrics')
-
-
-
-
-if __name__ == '__main__':
+@worker_process_init.connect()
+def on_worker_init(**_):
+    global sess
     sess = gpt2.start_tf_sess()
     gpt2.load_gpt2(sess)
+
+@celery.task
+def get_lyric(prefix):
+    lyrics = gpt2.generate(sess, prefix=prefix, temperature=0.4, nsamples=4, length=50, batch_size=10, return_as_list=True,
+        truncate="<|endoftext|>", include_prefix=False)
+    return lyrics
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    data = request.get_json(force=True)
+    prefix = data['lyrics'].strip()
+    print(prefix)
+    j = {}
+    lyric = get_lyric.apply_async(args=[prefix]).get()
+    lyric = [l[l.find(prefix)+len(prefix):] for l in lyric]
+    lyric = [l.replace('<|startoftext|>', '') for l in lyric]
+    j['lyrics'] = lyric
+    return jsonify(j)
+
+if __name__ == '__main__':
     app.run(port='5002')
 
